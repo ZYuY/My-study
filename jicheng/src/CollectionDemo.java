8d37212abaa828952e5620d8ac060b64bc3a5d41
zyy <www.1833632698@qq.com>
2019-09-22 16:41:46 +0800

    2019·9·22

---
+++ jicheng/src/CollectionDemo.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collection;
+public class CollectionDemo {
+    Collection<String> list=new ArrayList<>();
+    System.out.println(list);
+    System.out.println(list.size());
+    System.out.println
+}
