0e2a89b002c25d1e358bb5b04e719f7f48ed92e0
ZYuY <1833632698@qq.com>
2019-10-22 22:56:40 +0800

    2019-10-22-剑指练习

---
+++ 2019-10-22-剑指/src/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class Solution {
+    //求二进制中1的个数
+    public int NumberOf1(int n) {
+        int t=0;
+        char[]ch=Integer.toBinaryString(n).toCharArray();
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]=='1'){
+                t++;
+            }
+        }
+        return t;
+    }
+    //用两个栈实现队列
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+        int first=stack2.pop();
+        while(!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+        return first;
+    }
+}
