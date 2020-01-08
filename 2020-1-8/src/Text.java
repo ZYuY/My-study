796bb7cdb4a792e10276311ef0337103c1e9083d
ZYuY <1833632698@qq.com>
2020-01-08 21:21:11 +0800

    2020-1-8-算法题

---
+++ 2020-1-8/src/Text.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class Text {
+        public int FindGreatestSumOfSubArray(int[] array) {
+            List<Integer> list = new ArrayList<>();
+            for(int i=0;i<array.length;i++){
+                int sum = 0;
+                for(int j=i;j<array.length;j++){
+                    sum += array[j];
+                    list.add(sum);
+                }
+            }
+            if(list.size() <=0) return 0;
+            Collections.sort(list);
+            return list.get(list.size()-1);
+        }
+    }
