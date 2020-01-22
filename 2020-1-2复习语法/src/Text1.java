0285b2d6d7fc1613208ddd83ef2bf4b328170df3
ZYuY <1833632698@qq.com>
2020-01-22 22:08:31 +0800

    2020-1-22-Java语法复习day1

---
+++ 2020-1-2复习语法/src/Text1.java
@@ -0,0 +1,26 @@
+public class Text1 {
+    public static void main(String[] args) {
+//        String str = "result = ";
+//        int a = 10;
+//        int b = 20;
+//        String result = str + a + b;
+//        System.out.println(result);
+//        System.out.println(a+b+"+"+a+b);
+//        System.out.println();
+        byte a = 10;
+        byte b = 20;
+        /**
+        //byte c = a + b;
+         byte a = 10;
+         byte b = 20;
+         byte c = a + b;
+         System.out.println(c);
+         虽然 a 和 b 都是 byte, 但是计算 a + b 会先将 a 和 b 都
+         提升成 int, 再进行计算, 得到的结果也是 int, 这是赋给 c, 就会出现上述错误
+         byte 和 short 这种低于
+         4 个字节的类型, 会先提升成 int, 再参与计算
+         **/
+        System.out.println();
+        //switch 中的值只能是 整数|枚举|字符|字符串
+    }
+}
