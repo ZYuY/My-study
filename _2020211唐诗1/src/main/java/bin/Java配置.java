e4848dd4f3b8672b22f3eed421e1d214e6fbf578
ZYuY <1833632698@qq.com>
2020-02-11 21:13:04 +0800

    2020-2-11-项目简单复习1

---
+++ _2020211唐诗1/src/main/java/bin/Java配置.java
@@ -0,0 +1,21 @@
+package bin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Java配置 {
+    public static void main(String[] args) throws IOException {
+        InputStream is=Java配置 .class.getClassLoader()
+                .getResourceAsStream("some.properties");
+        /**
+         * SomeClass.class.getClassLoader().getResourceAsStream()
+         * 相对于类编译出的类所在的根路径找的
+         */
+        Properties properties=new Properties();
+        properties.load(is);
+        String v=properties.getProperty("mysql.host");
+        System.out.println(v);
+    }
+}
+
