e01a45f83fa0c7196f1c4c730e171af46fbb2dc2
ZYuY <1833632698@qq.com>
2020-06-22 22:09:18 +0800

    20200622算法练习

---
+++ 20200622算法/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public double myPow(double x, int n) {
+        double result=1.0;
+        for(int i=n;i!=0;i/=2){
+            if(i%2!=0)
+                result*=x;
+            x*=x;
+        }
+        return n>0?result:1/result;
+    }
+}
